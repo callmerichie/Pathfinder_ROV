3e32bb014eea926559d9d659fecafd7bfd1afa80
callmerichie <bertoli.r@live.it>
2025-12-02 10:56:14 +0100

    Photo and Video Testing Remotely

--- Pi/picamera_test.py
+++ Pi/picamera_test.py
@@ -1,17 +1,24 @@
-#testing image and video
+#testing image and video remotely
 
 from picamera2 import Picamera2, Preview
 import time
 
 picamera = Picamera2()
-camera_config = picamera.create_config()
+
+#photo
+camera_config = picamera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
 picamera.configure(camera_config)
-picamera.start_preview(Preview.DRM)
+
 picamera.start()
-
 time.sleep(5)
-
 picamera.capture_file('test_picamera.jpg')
-picamera.start_and_record_video('test_video_picamera.mp4',duration=10)
+picamera.stop()
 
+#video
+camera_config = picamera.create_video_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+picamera.configure(camera_config)
+
+picamera.start()
+time.sleep(5)
+picamera.start_and_record_video('test_video_picamera.mp4',duration=10)
 picamera.stop()
